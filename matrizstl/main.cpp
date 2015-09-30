99d1b1dc4d97cec4dd2c3fe915a95918390ba051
agostinhobritojr <agostinhobritojr@gmail.com>
2015-09-30 08:33:16 -0300

    stl

--- matrizstl/main.cpp
+++ matrizstl/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-  cout << "Hello World!" << endl;
+int main(){
+  vector< vector<int> > matriz(5,vector<int>(6,0));
+
+  vector< vector<int> > matriz1;
+
+  vector<int> v(10);
+
+  matriz1.push_back(v);
+
+  for(int i=0; i<matriz.size(); i++){
+    for(int j=0; j<matriz[i].size(); j++){
+      cout << matriz[i][j] << " ";
+    }
+    cout << endl;
+  }
   return 0;
 }
 
